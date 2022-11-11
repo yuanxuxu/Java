a42a30c310cf91d075f38f4e95397612e2d5233a
Yuanxu Xu <yuanxu.xu@hotmail.com>
2022-11-11 14:33:10 -0600

    created Java repo

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> names = Lists.newArrayList("John", "Adam", "Jane");
+    }
+}
